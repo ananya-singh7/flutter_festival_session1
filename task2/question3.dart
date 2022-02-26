36ebe92b0d993405ab21786356a592a6daba0d17
ananya-singh7 <89238834+ananya-singh7@users.noreply.github.com>
2022-02-26 20:49:32 +0530

    Create question3.dart

---
+++ task2/question3.dart
@@ -0,0 +1,11 @@
+import 'dart:io';
+ 
+void main()
+{
+  var map = {'Ananya' : '11th April', 'Aditi' : '23rd December', 'Archi' : '12th March' , 'Saumya' : '15th November', 'Aditya' : ' 7th July'};
+  String? name = stdin.readLineSync();
+  if(map.containsKey(name) == true)
+     print("${name}'s Birthday is on ${map[name]}");
+  else
+     print("Wrong Input: Given name not found in the dictionary");
+}
