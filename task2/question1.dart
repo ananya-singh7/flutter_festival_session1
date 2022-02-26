b23cb6c4b317b8a6e62cd8540234b41b3a5ecfd9
ananya-singh7 <89238834+ananya-singh7@users.noreply.github.com>
2022-02-26 20:33:38 +0530

    Update and rename q1.dart to question1.dart

---
+++ task2/question1.dart
@@ -0,0 +1,71 @@
+i) Which function declaration is correct?
+a. int multiply(a, b) => { return a * b; }
+b. int multiply(int a, int b) => return a * b;
+c. int multiply(int a, int b) => a * b; 
+
+Ans.  c. int multiply(int a, int b) => a * b; 
+
+
+
+ii) The await keyword works only
+1.  sync functions.
+2.  Async functions 
+3.  Anonymous functions
+4.  All of the above
+
+Ans.  2. Async functions 
+  
+  
+iii) Select an ordered collection in dart
+1.  Map
+2.  Set
+3. 	List 
+4. 	None
+
+Ans.  3. List 
+  
+  
+  
+iv) Select the correct output of the following dart expression.
+        	‘${“word”.toUpperCase()}’
+
+1.   The output is –‘WORD’ 
+2.   The output is ‘word’
+3.   Error generated
+4. 	 none
+  
+Ans.  1. The output is –‘WORD’ 
+  
+  
+  
+v) If a variable name is declared in Dart as: var name = “Me”; what does not uses string interpolation correctly?
+1.   print(“Your name is $name”);
+2.   print(‘Your name is ${name}’);
+3.   print(‘Your name is ‘{name}’); 
+4.   None
+  
+Ans.  3. print(‘Your name is ‘{name}’); 
+
+
+
+vi) Select the correct statement if the following is provided: var nums = [1,2,3,4];
+1.	nums.forEach(print());
+2.	nums.forEach(print); 
+3.	forEach(print);
+4.	nums(print(n.length);
+
+Ans.  2. nums.forEach(print);
+         
+         
+         
+vii) Advantages of using generics are:(There can be multiple correct answer for this question)
+1.  Ensures type safety
+2.  Reduce code duplication
+3.  One has to make different interfaces i.e for each data type we have to make different interface
+4.  Can work in a single interface that takes a type parameter.
+  
+Ans.  1.  Ensures type safety
+      2.  Reduce code duplication
+      4.  Can work in a single interface that takes a type parameter.
+
+
